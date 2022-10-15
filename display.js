b972eb80e3c014df34691d744b6bbdea818607f7
Ujjwal Pandey <ujjwalpandey24@gmail.com>
2022-10-15 15:37:44 +0530

    form submission

--- display.js
+++ display.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+const nameElement = document.getElementsByClassName("name")[0];
+const emailElement = document.getElementsByClassName("email")[0];
+
+const myKeysValues = window.location.search;
+const urlParams = new URLSearchParams(myKeysValues);
+
+const firstName = urlParams.get("firstname");
+const lastName = urlParams.get("lastname");
+const email = urlParams.get("email");
+
+// nameElement.insertAdjacentText("","Hello")
+nameElement.insertAdjacentText("beforebegin",`Hello, ${firstName} ${lastName}`)
+emailElement.insertAdjacentText("beforebegin",`Email: ${email}`)
